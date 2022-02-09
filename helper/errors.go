64503bad4d14edab0c2ff7fe761c505c885e7f83
khilmi aminudin <khilmiaminudin715@gmail.com>
2022-02-09 16:05:04 +0700

    edit helper/errors.go file

--- helper/errors.go
+++ helper/errors.go
@@ -1,7 +1,23 @@
 package helper
 
+import "github.com/sirupsen/logrus"
+
 func LogErrorAndPanic(err error) {
 	if err != nil {
 		Logger().Panic(err.Error())
+	}
+}
+
+func LogErrorWithFields(err error, fieldName string, value interface{}) {
+	if err != nil {
+		Logger().WithFields(logrus.Fields{
+			fieldName: value,
+		}).Error(err.Error())
+	}
+}
+
+func LogError(err error) {
+	if err != nil {
+		Logger().Error(err.Error())
 	}
 }
