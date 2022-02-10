07847a654de45bad9b41b29be70b8df10acfcc2e
khilmi aminudin <khilmiaminudin715@gmail.com>
2022-02-10 10:05:06 +0700

    fix actor service

--- service/actor_service.go
+++ service/actor_service.go
@@ -3,18 +3,22 @@ package service
 import (
 	"context"
 	"database/sql"
+	"dvdrental/helper"
+	"dvdrental/models/entity"
 	"dvdrental/models/request"
+	"dvdrental/models/responses"
 	"dvdrental/repository"
+	"net/http"
 
 	"github.com/go-playground/validator/v10"
 )
 
 type ActorService interface {
-	Create(ctx context.Context, request request.ActorCreateRequest)
-	Update()
-	Delete()
-	Find()
-	FindAll()
+	Create(ctx context.Context, request request.ActorCreateRequest) responses.ApiResponse
+	Update(ctx context.Context, request request.ActorUpdateRequest) responses.ApiResponse
+	Delete(ctx context.Context, actorID int64) responses.ApiResponse
+	Find(ctx context.Context, actorID int64) responses.ApiResponse
+	FindAll(ctx context.Context) responses.ApiResponse
 }
 
 type actorService struct {
@@ -31,22 +35,95 @@ func NewActorService(repo repository.ActorRepository, dbConn *sql.DB, validator 
 	}
 }
 
-func (service *actorService) Create(ctx context.Context, request request.ActorCreateRequest) {
-	panic("NY Implemented")
+func (service *actorService) Create(ctx context.Context, request request.ActorCreateRequest) responses.ApiResponse {
+	err := service.validate.Struct(request)
+
+	if isError := helper.LogErrorWithFields(err, "createActorParams", request); isError {
+		return responses.ApiResponse{
+			Code:   http.StatusBadRequest,
+			Status: "Bad Request",
+			Data:   "Cannot convert request body or wrong request body format",
+		}
+	}
+
+	tx, err := service.db.Begin()
+	_ = helper.LogError(err)
+
+	actor := service.repository.Create(ctx, tx, entity.Actor{FirstName: request.FirstName, LastName: request.LastName})
+	return responses.ApiResponse{
+		Code:   http.StatusOK,
+		Status: "Success",
+		Data:   actor,
+	}
 }
 
-func (service *actorService) Update() {
-	panic("NY Implemented")
+func (service *actorService) Update(ctx context.Context, request request.ActorUpdateRequest) responses.ApiResponse {
+	err := service.validate.Struct(request)
+
+	if isError := helper.LogErrorWithFields(err, "updata_actor_params", request); isError {
+		return responses.ApiResponse{
+			Code:   http.StatusBadRequest,
+			Status: "Bad Request",
+			Data:   "Cannot convert request body or wrong request body format",
+		}
+	}
+
+	tx, err := service.db.Begin()
+	_ = helper.LogError(err)
+
+	actor := service.repository.Update(ctx, tx, entity.Actor{ActorID: request.ActorID, FirstName: request.FirstName, LastName: request.LastName})
+
+	return responses.ApiResponse{
+		Code:   http.StatusOK,
+		Status: "Success",
+		Data:   actor,
+	}
 }
 
-func (service *actorService) Delete() {
-	panic("NY Implemented")
+func (service *actorService) Delete(ctx context.Context, actorID int64) responses.ApiResponse {
+	tx, err := service.db.Begin()
+	_ = helper.LogError(err)
+
+	err = service.repository.Delete(ctx, tx, entity.Actor{ActorID: actorID})
+
+	if helper.LogError(err) {
+		return responses.ApiResponse{
+			Code:   http.StatusBadRequest,
+			Status: "Bad Request",
+			Data:   err.Error(),
+		}
+	}
+
+	return responses.ApiResponse{
+		Code:   http.StatusOK,
+		Status: "Success",
+		Data:   err,
+	}
+
 }
 
-func (service *actorService) Find() {
-	panic("NY Implemented")
+func (service *actorService) Find(ctx context.Context, actorID int64) responses.ApiResponse {
+	tx, err := service.db.Begin()
+	_ = helper.LogError(err)
+
+	actor := service.repository.Find(ctx, tx, entity.Actor{ActorID: actorID})
+
+	return responses.ApiResponse{
+		Code:   http.StatusOK,
+		Status: "Success",
+		Data:   actor,
+	}
 }
 
-func (service *actorService) FindAll() {
-	panic("NY Implemented")
+func (service *actorService) FindAll(ctx context.Context) responses.ApiResponse {
+	tx, err := service.db.Begin()
+	_ = helper.LogError(err)
+
+	actors := service.repository.FindAll(ctx, tx)
+
+	return responses.ApiResponse{
+		Code:   http.StatusOK,
+		Status: "Success",
+		Data:   actors,
+	}
 }
