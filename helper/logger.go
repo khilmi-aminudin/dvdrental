8bde963dc43c38f3334b963a2137bea70d8ddd92
khilmi aminudin <khilmiaminudin715@gmail.com>
2022-02-11 10:30:25 +0700

    modify logger function

--- helper/logger.go
+++ helper/logger.go
@@ -1,12 +1,19 @@
 package helper
 
-import "github.com/sirupsen/logrus"
+import (
+	"os"
+
+	"github.com/sirupsen/logrus"
+)
 
 var logger *logrus.Logger
 
 func init() {
 	logger = logrus.New()
-	// file, err := os.OpenFile("application.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	file, _ := os.OpenFile("application.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+
+	logger.SetLevel(logrus.DebugLevel)
+	logger.SetOutput(file)
 }
 
 func Logger() *logrus.Logger {
