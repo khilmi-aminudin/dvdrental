5d538576cf02439da2d9c2fb2a0e39caf655104c
khilmi aminudin <khilmiaminudin715@gmail.com>
2022-02-14 14:54:31 +0700

    add LogTrace function

--- helper/logger.go
+++ helper/logger.go
@@ -12,10 +12,14 @@ func init() {
 	logger = logrus.New()
 	file, _ := os.OpenFile("application.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 
-	logger.SetLevel(logrus.DebugLevel)
+	logger.SetLevel(logrus.TraceLevel)
 	logger.SetOutput(file)
 }
 
 func Logger() *logrus.Logger {
 	return logger
+}
+
+func LogTrace(msg interface{}) {
+	logger.Trace(msg)
 }
