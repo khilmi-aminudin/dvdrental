d196f90712f6c362a2f22f059acbc9eff3b82b12
khilmi aminudin <khilmiaminudin715@gmail.com>
2022-02-14 14:58:12 +0700

    modify actor service to implementation redis

--- service/actor_service.go
+++ service/actor_service.go
@@ -3,12 +3,15 @@ package service
 import (
 	"context"
 	"database/sql"
+	"dvdrental/app/redisdata"
 	"dvdrental/helper"
 	"dvdrental/models/entity"
 	"dvdrental/models/request"
 	"dvdrental/models/responses"
 	"dvdrental/repository"
+	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/go-playground/validator/v10"
 )
@@ -26,6 +29,8 @@ type actorService struct {
 	db         *sql.DB
 	validate   *validator.Validate
 }
+
+var actorData []entity.Actor
 
 func NewActorService(repo repository.ActorRepository, dbConn *sql.DB, validator *validator.Validate) ActorService {
 	return &actorService{
@@ -108,6 +113,30 @@ func (service *actorService) Delete(ctx context.Context, actorID int64) response
 }
 
 func (service *actorService) Find(ctx context.Context, actorID int64) responses.ApiResponse {
+	// tx, err := service.db.Begin()
+	// _ = helper.LogError(err)
+
+	// defer helper.CommitOrRollback(tx)
+
+	// actor := service.repository.Find(ctx, tx, entity.Actor{ActorID: actorID})
+
+	// return responses.ApiResponse{
+	// 	Code:   http.StatusOK,
+	// 	Status: "Success",
+	// 	Data:   actor,
+	// }
+
+	for _, actor := range actorData {
+		if actor.ActorID == actorID {
+			helper.LogTrace("return data dari redis")
+			return responses.ApiResponse{
+				Code:   http.StatusOK,
+				Status: "Success",
+				Data:   actor,
+			}
+		}
+	}
+
 	tx, err := service.db.Begin()
 	_ = helper.LogError(err)
 
@@ -115,6 +144,7 @@ func (service *actorService) Find(ctx context.Context, actorID int64) responses.
 
 	actor := service.repository.Find(ctx, tx, entity.Actor{ActorID: actorID})
 
+	helper.LogTrace("return data dengan query")
 	return responses.ApiResponse{
 		Code:   http.StatusOK,
 		Status: "Success",
@@ -123,16 +153,44 @@ func (service *actorService) Find(ctx context.Context, actorID int64) responses.
 }
 
 func (service *actorService) FindAll(ctx context.Context) responses.ApiResponse {
-	tx, err := service.db.Begin()
-	_ = helper.LogError(err)
 
-	defer helper.CommitOrRollback(tx)
+	rds := redisdata.RedisClient()
 
-	actors := service.repository.FindAll(ctx, tx)
+	data, err := rds.Get(ctx, "actors").Result()
+	helper.LogErrorWithFields(err, "data_actor", data)
 
+	if err == redisdata.NoData {
+		// QUery to DB and stire to Redis
+		tx, err := service.db.Begin()
+		_ = helper.LogError(err)
+
+		defer helper.CommitOrRollback(tx)
+
+		actors := service.repository.FindAll(ctx, tx)
+
+		byteActors, err := json.Marshal(actors)
+		helper.LogError(err)
+
+		// store data to redis
+		err = rds.Set(ctx, "actors", byteActors, time.Minute*10).Err()
+		helper.LogError(err)
+
+		helper.LogTrace("return dengan query setelah set data  ke redis")
+		return responses.ApiResponse{
+			Code:   http.StatusOK,
+			Status: "Success",
+			Data:   actors,
+		}
+	}
+
+	err = json.Unmarshal([]byte(data), &actorData)
+	helper.LogError(err)
+
+	helper.LogTrace("return tanpa query ke database")
 	return responses.ApiResponse{
 		Code:   http.StatusOK,
 		Status: "Success",
-		Data:   actors,
+		Data:   actorData,
 	}
+
 }
